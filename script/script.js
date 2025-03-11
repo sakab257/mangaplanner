0f44e7cffc097207992713664f9903a1be14efc0
Salim Bouskine <salim.bouskine@dauphine.eu>
2025-03-11 02:31:47 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// responsive navabr
+function showSideBar(){
+  const sidebar = document.getElementById('mobile-sidebar');
+  sidebar.style.display = 'flex';
+}
+function hideSideBar(){
+  const sidebar = document.getElementById('mobile-sidebar');
+  sidebar.style.display = 'none';
+}
